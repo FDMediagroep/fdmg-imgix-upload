741d20b5213c0f6be3677c58b04e83016f9e0949
Willem Liu <willem.liu@fdmediagroep.nl>
2023-03-17 10:44:49 +0100

    Fix new glob signature

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -4,27 +4,21 @@ import { glob } from "glob";
  * of all the files in given location (dir).
  */
 export async function getFiles(dir: string) {
-  const promise = new Promise<{ fileName: string; absolutePath: string }[]>(
-    (resolve, reject) => {
-      const results: { fileName: string; absolutePath: string }[] = [];
-      glob(
-        `${dir}/**/*`,
-        { nodir: true, absolute: true },
-        function (er, files) {
-          if (er) {
-            reject();
-          }
-          files.forEach((absolutePath) => {
-            const pathSplits = absolutePath.split("/");
-            results.push({
-              fileName: pathSplits[pathSplits.length - 1],
-              absolutePath,
-            });
-          });
-          resolve(results);
-        }
-      );
-    }
-  );
-  return promise;
+  const results: { fileName: string; absolutePath: string }[] = [];
+  try {
+    const files = await glob(`${dir}/**/*`, {
+      nodir: true,
+      absolute: true,
+    });
+    files.forEach((absolutePath) => {
+      const pathSplits = absolutePath.split("/");
+      results.push({
+        fileName: pathSplits[pathSplits.length - 1],
+        absolutePath,
+      });
+    });
+  } catch (e) {
+    throw e;
+  }
+  return results;
 }
